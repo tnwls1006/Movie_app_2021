abd9c7e189e3b6a6b42727f00054b07bc05b54f9
tnwls1006 <sj170903@gmail.com>
2021-06-28 23:48:07 +0900

    https://github.com/tnwls1006/Movie_app_2021.git

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+function Food({ fav }){
+  return(
+    <h3>I Love { fav }</h3>
+  );
+}
+
 function App() {
   return (
     <div>
+      <h1>Hello!!</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
       </div>
      
   );
