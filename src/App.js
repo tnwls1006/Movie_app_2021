dea9d6aa94dfedd1e9d5ea4d4c1a7e7bacb5b098
tnwls1006 <sj170903@gmail.com>
2021-07-12 11:16:49 +0900

    https://github.com/tnwls1006/Movie_app_2021.git

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React from 'react';
-
-function Food({ fav }){
-  return(
-    <h3>I Love { fav }</h3>
-  );
-}
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello!!</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
-      </div>
-     
+    <HashRouter>
+          <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
   );
 }
 
